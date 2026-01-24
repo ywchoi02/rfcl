3c65282745d13a27418e25f6d79f47dd627d8fcc
ywchoi02 <83618525+ywchoi02@users.noreply.github.com>
2026-01-24 00:38:33 -0800

    Update maniskill_patch.py

--- rfcl/patches/maniskill_patch.py
+++ rfcl/patches/maniskill_patch.py
@@ -1,14 +1,19 @@
 """Monkey patch to fix ManiSkill3 flatten_state_dict failing on numpy.ndarray inputs."""
+from typing import Optional
+
 import numpy as np
 import torch
+
 from mani_skill.utils import common
+from mani_skill.utils.structs.types import Device
 
 _old_flatten = common.flatten_state_dict
 
-def patched_flatten_state_dict(state_dict):
-    return _old_flatten(
-        state_dict, 
-        use_torch=isinstance(state_dict, np.ndarray),
-    )
+def patched_flatten_state_dict(state_dict, use_torch=False, device: Optional[Device] = None):
+    use_torch = isinstance(state_dict, np.ndarray)
+    flat = _old_flatten(state_dict, use_torch=use_torch, device=device)
+    if use_torch and isinstance(flat, torch.Tensor):
+        flat = flat.cpu().detach().numpy()
+    return flat
 
 common.flatten_state_dict = patched_flatten_state_dict
